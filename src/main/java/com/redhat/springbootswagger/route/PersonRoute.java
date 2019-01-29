1a879343b4b5f9f5ebfa7ea060800c691eb7e4fc
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2019-01-29 10:03:40 -0500

    added routes and ids

--- src/main/java/com/redhat/springbootswagger/route/PersonRoute.java
+++ src/main/java/com/redhat/springbootswagger/route/PersonRoute.java
@@ -1,6 +1,7 @@
 package com.redhat.springbootswagger.route;
 
 import com.redhat.springbootswagger.model.Person;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,10 @@ public class PersonRoute extends RouteBuilder {
     public void configure() {
         restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
 
-        rest("/person").get().outType(Person.class)
+        rest("/person").get().id("restPerson").outType(Person.class)
                 .to("direct:talk");
 
-        from("direct:talk")
+        from("direct:talk").id("talkRoute")
                 .log("Processing exchange...")
                 .process(exchange -> {
                     Person p = new Person();
@@ -23,5 +24,11 @@ public class PersonRoute extends RouteBuilder {
                     p.setLastname("LastName");
                     exchange.getIn().setBody(p);
                 });
+
+        rest().get("/hello").id("restHello").to("direct:hello");
+
+        from("direct:hello").id("helloRoute")
+                .log(LoggingLevel.INFO, "Hello World")
+                .transform().simple("Hello World");
     }
 }
