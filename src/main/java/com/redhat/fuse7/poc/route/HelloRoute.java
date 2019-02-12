b22a497b047beba74efe0759f62a91aa8ca15a12
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2019-02-12 14:12:53 -0500

    changes

--- src/main/java/com/redhat/fuse7/poc/route/HelloRoute.java
+++ src/main/java/com/redhat/fuse7/poc/route/HelloRoute.java
@@ -21,13 +21,13 @@ public class HelloRoute extends RouteBuilder {
     @Override
     public void configure() {
         String greeterPrefixEnv = System.getenv().getOrDefault(GREETING_PREFIX, GREETING_PREFIX_DEFAULT_VALUE);
-        String greetingStr = String.format(greeterMessageFormat, greeterPrefixEnv) + " 444";
+        String greetingStr = String.format(greeterMessageFormat, greeterPrefixEnv) + " 1";
 
         rest().get("/hello").id("restHello").to("direct:hello").bindingMode(RestBindingMode.json);
 
         from("direct:hello").id("helloRoute")
-                .log(LoggingLevel.INFO, "4 env: " + greeterPrefixEnv)
-                .log(LoggingLevel.INFO, "4 file: " + greeterPrefix)
+                .log(LoggingLevel.INFO, "1 env: " + greeterPrefixEnv)
+                .log(LoggingLevel.INFO, "1 file: " + greeterPrefix)
                 .log(LoggingLevel.INFO, greetingStr)
                 .transform().simple(greetingStr);
     }
